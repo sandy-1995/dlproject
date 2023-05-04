9ae958d483a87e1f367c357a95161286dfcf56c3
sandy-1995 <sksanthosh1996@gmail.com>
2023-05-04 19:32:06 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="santhosh",
+    version="0.0.1",
+    author="sandy",
+    author_email="skanthosh1996@gmail.com",
+    description="A small python package for CNN app",
+    long_description_content="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
